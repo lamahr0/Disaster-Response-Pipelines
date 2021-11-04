eba4b9650b318b52a3e8778c0658725e356bd341
lamahr0 <41934760+lamahr0@users.noreply.github.com>
2021-11-05 02:34:30 +0300

    update save function

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -74,7 +74,7 @@ def save_data(df, database_filename):
         This is a description of what is returned.
     """
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('Messages', engine, index=False)
+    df.to_sql('Messages', engine, index=False , if_exists = 'replace')
 
 
 def main():
