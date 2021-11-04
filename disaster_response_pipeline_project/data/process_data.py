578fba3f8b0b5de4f6f24b04743a54d801f04f00
lamahr0 <41934760+lamahr0@users.noreply.github.com>
2021-11-05 02:37:50 +0300

    clean data

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -56,6 +56,7 @@ def clean_data(df):
     df = pd.concat([df , categories], axis = 1)
     # drop duplicates
     df = df.drop_duplicates()
+    df['related'] = df['related'].replace(2,0)
     
     
     return df 
